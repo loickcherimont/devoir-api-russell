638f492ebb5ee922184efdbd339b226c6cc5da2e
loickcherimont <loickcherimont@gmail.com>
2026-06-29 21:13:07 +0200

    feat: implement reservations routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userRoute from "./users.js";
 import catwayRoute from "./catways.js";
+import reservationRoute from "./reservations.js";
 
 const router = express.Router();
 
 router.use("/users", userRoute);
 router.use("/catways", catwayRoute);
+router.use("/catways/:catwayNumber/reservations", reservationRoute);
 
 export default router;
